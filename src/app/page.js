795f3672a15a1a9af679ba3d473eb0b56e3b9597
Mohan <mohanprasanth3883@gmail.com>
2025-03-26 14:14:46 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,175 @@
-import Image from "next/image";
+'use client'
+
+import { useForm } from "react-hook-form";
+import * as yup from "yup"
+import {yupResolver} from "@hookform/resolvers/yup"
+import { Eye, EyeOff } from 'lucide-react';
+import { useState } from "react";
+
+
+const schema = yup.object().shape({ // Defines the validation schema.
+  firstName : yup
+                .string()
+                .required('firstName is required'),
+
+  lastName : yup
+                .string()
+                .required('lastName is required'),
+
+  age : yup
+          .number()
+          .typeError('Only enter the number') 
+          .integer('please enter integer value')
+          .positive('please enter positive value')
+          .required('age is required'),
+
+  email : yup
+            .string()
+            .email('invalid email')
+            .required('email is required'),
+
+  password : yup
+              .string()
+              .trim() // Ensure no extra spaces are counted
+              .min(5,'password must be alteast 5 char')
+              .max(15)
+              .required('password is required'),
+
+  confirmPassword : yup
+                      .string()
+                      .trim() // Ensure no extra spaces are counted
+                      .oneOf([yup.ref("password")], 'incorrect password!')
+                      .required('confirmPassword is required'), //// Ensures it matches the "password" field.
+})
+
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const {register, handleSubmit,formState : {errors, isValid}, watch} = useForm({
+    resolver : yupResolver(schema), // Connects the schema to react-hook-form.
+    mode : 'onChange'
+  });
+
+  // console.log(errors)
+  console.log(isValid)
+
+
+  //password show and hide
+  const [showPasswrd,setShowPassword] = useState(false)
+  const [showConfirmPasswrd,setShowConfirmPassword] = useState(false)
+
+  //only show icons when i type in input field
+      //watch() function is used to track the current value of specific fields in real time
+  const password = watch("password");
+  const confirmPassword = watch("confirmPassword");
+
+
+  const onSubmitForm =(data) =>{
+    
+    alert(JSON.stringify(data,null,2)) //data,null,2 -> null,2 is  used for alert message show like list of key:value
+    console.log("Form Data:", data);
+  }
+
+
+  return (
+    <main className="w-full min-h-screen bg-violet-50 pt-10 font-serif">
+      <div className=" flex flex-col justify-center items-center">
+        <h1 className="font-extrabold text-4xl mb-2 text-violet-800">Form</h1>
+        <form
+          onSubmit={handleSubmit(onSubmitForm)}
+          className="w-[310px] flex flex-col justify-center items-center border-1 border-gray-500 p-3 rounded-lg"
+        >
+          <div className="flex flex-col w-full mb-1">
+            <label htmlFor="">F-Name :</label>
+            <input
+              {...register("firstName")}
+              className="w-full border border-gray-400  rounded p-1 h-10"
+              type="text"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            {errors?.firstName && <p className='text-red-500 text-sm'>{errors.firstName.message}</p>}
+          </div>
+
+          <div className="flex flex-col w-full mb-1">
+            <label htmlFor="">L-Name :</label>
+            <input
+              {...register("lastName")}
+              className="w-full border border-gray-400  rounded p-1 h-10"
+              type="text"
+            />
+            {errors?.lastName && <p className='text-red-500 text-sm'>{errors.lastName.message}</p>}
+          </div>
+
+          <div className="flex flex-col w-full mb-1">
+            <label htmlFor="">Age :</label>
+            <input
+              {...register("age")}
+              className="w-full border border-gray-400 rounded p-1 h-10"
+              type="text"
+            />
+            {errors?.age && <p className='text-red-500 text-sm'>{errors.age.message}</p>}
+          </div>
+
+          <div className="flex flex-col w-full mb-1 ">
+            <label htmlFor="">Email :</label>
+            <input
+              {...register("email")}
+              className="w-full border border-gray-400  rounded p-1 h-10"
+              type="text"
+            />
+            {errors?.email && <p className='text-red-500 text-sm'>{errors.email.message}</p>}
+          </div>
+
+          <div className="flex flex-col w-full mb-1 relative">
+            <label htmlFor="">Password :</label>
+            <div className="relative">
+              <input
+                {...register("password")}
+                className="w-full border border-gray-400  rounded p-1 h-10"
+                type={showPasswrd ? "text" : "password"}
+              />
+              {password &&
+                <span type="button" 
+                      onClick={()=>setShowPassword((pre)=> !pre)} 
+                      className="absolute bottom-2.5 right-1.5"
+                      >
+                        {showPasswrd ? <EyeOff size={18} /> : <Eye size={18} />}
+                </span> 
+              }
+            </div>
+           
+            {errors?.password && <p className='text-red-500 text-sm'>{errors.password.message}</p>}
+          </div>
+
+          <div className="flex flex-col w-full mb-1 ">
+            <label htmlFor="">Confirm Password :</label>
+            <div className="relative"> 
+              <input
+                {...register("confirmPassword")}
+                className="w-full border border-gray-400  rounded p-1 h-10"
+                type={showConfirmPasswrd ? "text" : "password"}
+              />
+              {confirmPassword && 
+                <span type="button" 
+                      onClick={()=> setShowConfirmPassword((pre)=>!pre)} 
+                      className="absolute bottom-2.5 right-1.5"
+                      >
+                        {showConfirmPasswrd ? <EyeOff size={18} /> : <Eye size={18} />}
+                </span>
+              }
+            </div>
+            {errors?.confirmPassword && <p className='text-red-500 text-sm'>{errors.confirmPassword.message}</p>}
+          </div>
+
+          <button
+            disabled={!isValid}
+            type="submit"
+            className={`border rounded flex justify-center items-center bg-violet-600 text-white font-extrabold m-3 text-center px-15 py-2 ${!isValid ? "disabled:bg-violet-300" : ""}`}
           >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            Submit
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
